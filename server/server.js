815b66229e732d6456ccf337b25b750c0ea67425
Gaurav Kimothi <gaurav.kimothi@gmail.com>
2018-06-06 13:09:20 -0700

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
@@ -29,27 +29,32 @@ io.on('connection', (socket) => {
 
     //socket.emit from Admin
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
+    // socket.emit('newMessage', {
+    //     from: 'Admin',
+    //     text: 'Welcome to the chat app',
+    //     createdAt: new Date().getTime()
+    // });
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     //socket.broadcast.emit from Admin
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    // socket.broadcast.emit('newMessage', {
+    //     from: 'Admin',
+    //     text: 'New user joined',
+    //     createdAt: new Date().getTime()
+    // });
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         //io.emit emits the same event to all connections
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
