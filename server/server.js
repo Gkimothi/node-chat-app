940fff697464445d30920d336fb8c3aee3eaca47
Gaurav Kimothi <gaurav.kimothi@gmail.com>
2018-06-06 12:43:23 -0700

    Greet new user and alert others'

--- server/server.js
+++ server/server.js
@@ -27,6 +27,21 @@ io.on('connection', (socket) => {
     //     text: 'What is going on?'
     // });
 
+    //socket.emit from Admin
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    //socket.broadcast.emit from Admin
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         //io.emit emits the same event to all connections
@@ -35,6 +50,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect', () => {
